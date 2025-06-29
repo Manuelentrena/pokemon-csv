a818f184e2645e5ea02284c21c2762b86f7618a2
entrenadev <manuel.entrena@gmail.com>
2025-06-29 16:23:08 +0200

    feat(enum): :sparkles: Added PokemonColor enum

---
+++ src/pokemon/domain/pokemon-color.enum.ts
@@ -0,0 +1,12 @@
+export enum PokemonColor {
+  BLACK = 'black',
+  BLUE = 'blue',
+  BROWN = 'brown',
+  GRAY = 'gray',
+  GREEN = 'green',
+  PINK = 'pink',
+  PURPLE = 'purple',
+  RED = 'red',
+  WHITE = 'white',
+  YELLOW = 'yellow',
+}
