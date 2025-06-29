fe5a8685dee35a5f885397b78dc1950973d26eec
entrenadev <manuel.entrena@gmail.com>
2025-06-29 17:53:56 +0200

    feat(repository): :sparkles: Enhanced PokeApiRepository to handle 404 errors by adding failed Pokémon names to the store

--- src/pokemon/infrastructure/pokeapi.repository.ts
+++ src/pokemon/infrastructure/pokeapi.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs';
 import { PokemonRepository } from '../domain/pokemon.repository';
@@ -10,10 +10,15 @@ import {
 } from './types/pokeapi.type';
 import { Pokemon } from '../domain/pokemon.entity';
 import { PokemonColor } from '../domain/pokemon-color.enum';
+import { isAxiosError } from 'axios';
+import { PokemonFailedStore } from '../application/services/pokemon-failed.store';
 
 @Injectable()
 export class PokeApiRepository implements PokemonRepository {
-  constructor(private readonly http: HttpService) {}
+  constructor(
+    private readonly http: HttpService,
+    private readonly failedStore: PokemonFailedStore,
+  ) {}
 
   async getAllNames(): Promise<PokemonNamesListDto> {
     const url = `https://pokeapi.co/api/v2/pokemon?limit=100000`;
@@ -31,16 +36,26 @@ export class PokeApiRepository implements PokemonRepository {
   async getOneByName(name: string): Promise<Pokemon> {
     const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
 
-    const { data } = await lastValueFrom(
-      this.http.get<PokemonApiResponse>(url),
-    );
+    try {
+      const { data } = await lastValueFrom(
+        this.http.get<PokemonApiResponse>(url),
+      );
 
-    return new Pokemon(
-      data.name,
-      data.height,
-      data.weight,
-      data.base_experience,
-    );
+      return new Pokemon(
+        data.name,
+        data.height,
+        data.weight,
+        data.base_experience,
+      );
+    } catch (error: unknown) {
+      if (isAxiosError(error)) {
+        if (error.response?.status === 404) {
+          this.failedStore.add(name);
+          throw new NotFoundException(`Pokemon "${name}" not found`);
+        }
+      }
+      throw error;
+    }
   }
 
   async getAllByColor(color: PokemonColor): Promise<string[]> {
