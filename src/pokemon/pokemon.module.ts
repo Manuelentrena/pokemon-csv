386de6b76b935b9ab79151b401a6d6a1cbce3843
entrenadev <manuel.entrena@gmail.com>
2025-06-29 13:24:27 +0200

    feat(modules): :building_construction: Setted up PokemonModule with imports, controllers, and providers

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,4 +1,26 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { PokeApiRepository } from './infrastructure/pokeapi.repository';
+import { PokemonRepository } from './domain/pokemon.repository';
+import { InitPokemonNamesListUseCase } from './application/init-pokemon-names-list.use-case';
+import { PokemonNamesListStore } from './application/services/pokemon-names-list.store';
+import { PokemonController } from './interfaces/Pokemon.controller';
+import { SearchPokemonByNameUseCase } from './application/search-pokemon-by-name.use-case';
+import { GetAllPokemonNamesUseCase } from './application/get-all-pokemon-names.use-case';
 
-@Module({})
+@Module({
+  imports: [HttpModule],
+  controllers: [PokemonController],
+  providers: [
+    PokeApiRepository,
+    InitPokemonNamesListUseCase,
+    SearchPokemonByNameUseCase,
+    GetAllPokemonNamesUseCase,
+    PokemonNamesListStore,
+    {
+      provide: PokemonRepository,
+      useClass: PokeApiRepository,
+    },
+  ],
+})
 export class PokemonModule {}
