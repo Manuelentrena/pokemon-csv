e2658940c5d2a8e4dc31e2d5bd161e926148de94
entrenadev <manuel.entrena@gmail.com>
2025-06-29 14:08:43 +0200

    feat(controllers): :sparkles: Enhanced PokemonController to support searching and retrieving multiple Pokemon by name

--- src/pokemon/interfaces/Pokemon.controller.ts
+++ src/pokemon/interfaces/Pokemon.controller.ts
@@ -2,17 +2,26 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { SearchPokemonByNameUseCase } from '../application/search-pokemon-by-name.use-case';
 import { GetAllPokemonNamesUseCase } from '../application/get-all-pokemon-names.use-case';
 import { FindByNameQueryDto } from './dto/find-by-name-query.dto';
+import { GetPokemonByNameUseCase } from '../application/get-pokemon-by-name.use-case';
 
 @Controller('pokemon')
 export class PokemonController {
   constructor(
     private readonly searchByName: SearchPokemonByNameUseCase,
     private readonly getAllNames: GetAllPokemonNamesUseCase,
+    private readonly getPokemonByName: GetPokemonByNameUseCase,
   ) {}
 
   @Get('findByName')
-  search(@Query() query: FindByNameQueryDto) {
-    return this.searchByName.execute(query.name);
+  async search(@Query() query: FindByNameQueryDto) {
+    const allNames = this.searchByName.execute(query.name);
+    const pokemons = await Promise.all(
+      allNames.results.map((name) => this.getPokemonByName.execute(name)),
+    );
+    return {
+      count: pokemons.length,
+      results: pokemons,
+    };
   }
 
   @Get('getAllNames')
